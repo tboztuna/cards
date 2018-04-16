ff778bb54a66360809c840fc4af7a560e17bf344
Tolga Boztuna <tboztuna@yahoo.com>
2018-04-16 16:20:48 +0300

    created & implemented deck type and newDeck function

--- main.go
+++ main.go
@@ -1,17 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
