dc192f3b82387d397d9cbbc7abfbb2bb3ca15730
Tolga Boztuna <tboztuna@yahoo.com>
2018-04-16 15:35:27 +0300

    print all cards in a deck

--- main.go
+++ main.go
@@ -3,6 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of Spades"
-	fmt.println(card)
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
