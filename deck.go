121c57aba46d7eb71538a633db0cb394ed38ac30
Tolga Boztuna <tboztuna@yahoo.com>
2018-04-19 08:59:42 +0300

    saveToFile() function added to deck type

--- deck.go
+++ deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -45,4 +46,8 @@ func (d deck) toString() string {
 
 	return strings.Join([]string(d), ",")
 
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
